067dfe358ab4202ff1611de1c932e5d2796a15f4
hafsaaaaaaaaaa <hafsashah415@gmail.com>
2026-07-17 15:02:24 +0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+// File location: build.gradle.kts (ROOT of project)
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.7.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
