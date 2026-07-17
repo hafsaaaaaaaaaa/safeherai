1186bba9c20a6b34acb9a71a2f682e278a1b715c
hafsaaaaaaaaaa <hafsashah415@gmail.com>
2026-07-17 15:01:23 +0500

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+// File location: settings.gradle.kts
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SafeHer AI"
+include(":app")
